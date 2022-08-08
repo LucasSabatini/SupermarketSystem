071c25b0039b2510c9d0bfcfc391af42eec18f44
lusab <lucas_sabatini@hotmail.com>
2022-08-08 15:11:00 -0300

    Implementação da classe de menu do usuário

--- src/supermarket/system/stock/MenuDoUsuario.java
+++ src/supermarket/system/stock/MenuDoUsuario.java
@@ -1,19 +1,63 @@
 package supermarket.system.stock;
 
-import supermarket.system.SuperMarketApp;
-
 import java.util.Scanner;
 
 public class MenuDoUsuario {
 
-    public static void openUserMenu(){
+    public void openUserMenu(){
         int opcao; //Opção do menu
-        String nomeDoProduto,validade;
+        String nomeDoProduto,validade, valorDigitado;
         double valor;
         int quantidade;
 
+        new Produtos();
+        Produtos novoProduto; //Construção do objeto novoProduto do tipo(classe) Produtos
+        Estoque novoEstoque = new Estoque(); //Construção do objeto estoque
         Scanner sc = new Scanner(System.in); //Construção do objeto sc para utilizar dados de entrada via teclado
 
+        do {
+            //Menu do usuário
+            System.out.println("\n----------------------");
+            System.out.println("        MENU");
+            System.out.println("----------------------");
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Cadastrar um novo produto");
+            System.out.println("2 - Listar produtos do estoque");
+            System.out.println("3 - Alterar um produto do estoque");
+            System.out.println("4 - Excluir um produto do estoque");
+            System.out.println("0 - Sair");
+
+            opcao = sc.nextInt(); //Ler a opção do usuário
+            sc.nextLine(); //Limpar o buffer do teclado
+
+            switch (opcao) {
+                case 1 -> {
+                    System.out.println("Vamos cadastrar um novo produto!");
+                    System.out.print("Digite o nome: ");
+                    nomeDoProduto = sc.nextLine();
+                    System.out.print("Digite o valor: R$");
+                    valorDigitado = sc.nextLine();
+                    valorDigitado = valorDigitado.replaceAll(",", "."); //Substituir a virgula digitada para separar decimal por ponto para evitar erro de execução
+                    valor = Double.parseDouble(valorDigitado); //Converter o valor digitado de string para double
+                    System.out.print("Digite a quantidade: ");
+                    quantidade = sc.nextInt();
+                    sc.nextLine(); //Limpar o buffer do teclado
+                    System.out.print("Digite a data de validade: ");
+                    validade = sc.nextLine();
+
+                    novoProduto = new Produtos(nomeDoProduto, valor, quantidade, validade);
+
+                    novoEstoque.inserirProduto(novoProduto);
+                }
+                case 2 -> {
+                    novoEstoque.listarProdutos();
+                }
+                case 3 -> System.out.println("Escolha um produto do seu estoque para alterar: ");
+                case 4 -> System.out.println("Escolha um produto para excluir de seu estoque: ");
+                case 0 -> System.out.println("\nSaindo...");
+                default -> System.out.println("Opção inválida!");
+            }
+        }while(opcao != 0);
 
     }
 }
---
+++ src/supermarket/system/stock/SuperMarketApp.java
@@ -0,0 +1,22 @@
+package supermarket.system.stock;
+
+import java.util.Scanner;
+
+public class SuperMarketApp{
+
+    public static void main(String[] args) {
+
+        String nomeDoProduto,validade;
+        String valorDigitado;
+        double valor;
+        int quantidade, opcao;
+
+        Scanner sc = new Scanner(System.in); //Construção do objeto sc para utilizar dados de entrada via teclado
+        MenuDoUsuario abrirMenu = new MenuDoUsuario(); //Instância que permite chamar o método para abrir o menu
+
+        abrirMenu.openUserMenu(); //Chamada do método para abrir o menu
+
+
+    }
+
+}
